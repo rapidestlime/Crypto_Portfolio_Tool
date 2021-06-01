7935da7f83f493fce38dd88b05ba590bde2f99d3
o-ray-o <rayfong_goh@mymail.sutd.edu.sg>
2021-06-01 18:45:40 +0800

    log in works

--- app.R
+++ app.R
@@ -1,15 +1,62 @@
 source("usePackages.R")
-pkgnames <- c("tidyverse","shiny","DBI","jsonlite","shinydashboard","shinyWidgets","lubridate","hash")
+pkgnames <- c("tidyverse","shiny","DBI","jsonlite","shinydashboard","shinyWidgets","lubridate","hash","RSQLite")
 loadPkgs(pkgnames)
 
+# registerModal <- function(pwfailed = FALSE, uniquenamefailed = FALSE) {
+#   modalDialog(
+#     title = "Register a new account",
+#     textInput('playername', "Enter name:"),
+#     passwordInput("password1", "Enter a new password:"),
+#     passwordInput("password2", "Confirm by re-entering the new password:"),
+#     if (pwfailed)
+#       div(tags$b("The passwords do not match. Try again.", style = "color: red;")),
+#     if (uniquenamefailed)
+#       div(tags$b("The playername is in use. Try again.", style = "color: red;")),
+#     
+#     footer = tagList(
+#       modalButton("Cancel"),
+#       actionButton("registerok", "OK")
+#     )
+#   )
+# }
 
+loginModal <- function(failed = FALSE) {
+  modalDialog(
+    title = "Login",
+    textInput("username", "Enter your name:"),
+    passwordInput("password", "Enter your password:"),
+    if (failed)
+      div(tags$b("There is no registered player with that name and password. Try again.", style = "color: red;")),
+    footer = tagList(
+      modalButton("Cancel"),
+      actionButton("loginok", "OK")
+    )
+  )
+}
 
-
+# checkUniquePlayername <- function(playername){
+#   #open the connection
+#   conn <- dbConnect(RSQLite::SQlite(),dbpath)
+#   #password could contain an SQL insertion attack
+#   #Create a template for the query with placeholders for playername and password
+#   querytemplate <- "SELECT * FROM riskplayer WHERE playername=?id1;"
+#   query<- sqlInterpolate(conn, querytemplate,id1=playername)
+#   result <- dbGetQuery(conn,query)
+#   print(nrow(result))
+#   # If the query is successful, result should be a dataframe with one row
+#   if (nrow(result)==0){
+#     FALSE
+#   } else {
+#     TRUE
+#   }
+#   #Close the connection
+#   dbDisconnect(conn)
+# }
 
 
 #######ui########
 ui <- dashboardPage(skin= "purple",
-  dashboardHeader(title = "INEVITABLE FUND TRACKING TOOL",
+  dashboardHeader(title = "LFG PORTFOLIO TRACKING TOOL",
                   titleWidth = 450
                   ),
   dashboardSidebar(
@@ -78,7 +125,7 @@ ui <- dashboardPage(skin= "purple",
                     p('GOH RAY FONG'),
                     p('NICHOLAS TAN YI DA')
                   ),
-                  actionButton("register", "Register"),
+          
                   actionButton("login", "Login"),
                   tags$h4("Logged in as:")
                   #htmlOutput("loggedInAs", style="font-size:30;color:#D6AC18;font-family:Times New Roman;align:center;")
@@ -95,10 +142,45 @@ ui <- dashboardPage(skin= "purple",
 ################SERVER####################
 server <- function(input, output, session) {
   #reactiveValues objects for storing items like the user password
-  vals <- reactiveValues(password = NULL,playerid=NULL,playername=NULL,question_store = c(1:10),wrong_qns=c(),score=0,grid=list(0,0,0,0,0,0,0,0,0),query=NULL)
+  vals <- reactiveValues(password = NULL,clientid=NULL,playername=NULL,question_store = c(1:10),wrong_qns=c(),score=0,grid=list(0,0,0,0,0,0,0,0,0),query=NULL)
   timer <- reactiveVal(35)
   active <- reactiveVal(FALSE)
-  
+ 
+  # #Fire some code if the user clicks the Register button
+  # observeEvent(input$register, {
+  #   showModal(registerModal())
+  # })
+  # # Fire some code if the user clicks the registerok button
+  # observeEvent(input$registerok, {
+  #   # Check that password1 exists and it matches password2
+  #   if (nchar(input$password1)==0 || (input$password1 != input$password2)) 
+  #     showModal(registerModal(pwfailed = TRUE))
+  #   if (checkUniquePlayername(input$playername)) 
+  #     showModal(registerModal(uniquenamefailed = TRUE))
+  #   #store the password and close the dialog
+  #   vals$password <- input$password1
+  #   print(vals$password) # for debugging
+  #   vals$playername <- input$playername
+  #   print(vals$playername)
+  #   registerPlayer(vals$playername,vals$password)
+  #   if (!is.null(vals$playername))
+  #     vals$playerid <- getPlayerID(vals$playername,vals$password)
+  #   print(vals$playerid) # for debugging
+  #   removeModal()
+  #   
+  # })
+  #Fire some code if the user clicks the Login button
+  observeEvent(input$login, {
+    showModal(loginModal(failed=FALSE))
+  })
+  # Fire some code if the user clicks the loginok button
+  observeEvent(input$loginok, {
+    if (input$username=="dennis" && input$password=="abc") {
+      removeModal()
+    } else {
+      showModal(loginModal(failed = TRUE))
+    }
+  }) 
 }
 
 

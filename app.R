d5c3685fedb389e5d1a90217bd7e668de6598304
o-ray-o <rayfong_goh@mymail.sutd.edu.sg>
2021-06-02 16:45:04 +0800

    changing of tabs work after log in

--- app.R
+++ app.R
@@ -26,12 +26,33 @@ loginModal <- function(failed = FALSE) {
     textInput("username", "Enter your name:"),
     passwordInput("password", "Enter your password:"),
     if (failed)
-      div(tags$b("There is no registered player with that name and password. Try again.", style = "color: red;")),
+      div(tags$b("There is no registered person with that name and password. Try again.", style = "color: red;")),
     footer = tagList(
       modalButton("Cancel"),
       actionButton("loginok", "OK")
     )
   )
+}
+
+afterloginModal <- function(failed= FALSE){
+  modalDialog(
+    title="Welcome Back",
+    tags$br(),
+    "What do you want to do today?",
+    tags$br(),
+    fluidRow(column(3,
+                    actionLink("onboardclients", "On Board Clients")),
+             column(3,
+                    actionLink("marketoverview"," Market Overview")),
+             column(3,
+                    actionLink("viewportfolio", "View Portfolio")),
+             column(3,
+                    actionLink("updateportfolio", "Update Portfolio"))),
+    tags$br(),
+    footer = tagList(
+      modalButton("Close")
+    
+  ))
 }
 
 # checkUniquePlayername <- function(playername){
@@ -60,11 +81,11 @@ ui <- dashboardPage(skin= "purple",
                   titleWidth = 450
                   ),
   dashboardSidebar(
-    sidebarMenu(  
+    sidebarMenu(id="tabs",
       #https://fontawesome.com/icons?d=gallery
       menuItem("Welcome", tabName = "welcome", icon = icon("door-open")),
       menuItem("Onboarding", tabName = "onboarding", icon = icon("portal-enter")),
-      #menuItem("Market Overview", tabName = "market", icon = icon("binoculars")),
+      menuItem("Market Overview", tabName = "market", icon = icon("binoculars")),
       menuItem("Portfolio", tabName = "portfolio", icon = icon("chart-pie"))
     )
   ),
@@ -94,54 +115,135 @@ ui <- dashboardPage(skin= "purple",
                      height: 50%;
                      align: center;'
                   ),
-                  # tags$br(),
-                  # tags$h1(
-                  #   'WELCOME!',
-                  #   style='color: #000000;
-                  #          font-family: Times New Roman;
-                  #          align: center;'
-                  # ),
-                  # #tags$br(),
-                  # tags$h2(
-                  #   'About Tool',
-                  #   style='color: #000000;
-                  #          font-family: Times New Roman;
-                  #          align: center;'
-                  # ),
-                  # tags$p(
-                  #   "This tool allows one to keep track of the current crypto market as well as managing portfolio",
-                  #   style='color:#000000;
-                  #          font-family:Times New Roman;
-                  #          align:center;'
-                  # ),
-                  # 
-                  # tags$h2(
-                  #   "Creators:",
-                  #   style='color:#000000;
-                  #          font-family:Times New Roman;
-                  #          align:center;'
-                  # ),
-                  # tags$ul(
-                  #   p('GOH RAY FONG'),
-                  #   p('NICHOLAS TAN YI DA')
-                  # ),
-                  fluidRow(
-                    box(width=3,
-                        tags$h4("Logged in as:"),
-                        htmlOutput("loggedInAs"),
-                    ),
+                  tags$br(),
+                  tags$h1(
+                    'WELCOME!',
+                    style='color: #000000;
+                           font-family: Times New Roman;
+                           align: center;'
+                  ),
+                  #tags$br(),
+                  tags$h2(
+                    'About Tool',
+                    style='color: #000000;
+                           font-family: Times New Roman;
+                           align: center;'
+                  ),
+                  tags$p(
+                    "This tool allows one to keep track of the current crypto market as well as managing portfolio",
+                    style='color:#000000;
+                           font-family:Times New Roman;
+                           align:center;'
+                  ),
+
+                  tags$h2(
+                    "Creators:",
+                    style='color:#000000;
+                           font-family:Times New Roman;
+                           align:center;'
+                  ),
+                  tags$ul(
+                    p('GOH RAY FONG'),
+                    p('NICHOLAS TAN YI DA')
+                  ),
+                  # fluidRow(
+                  #   box(width=3,
+                  #       tags$h4("Logged in as:"),
+                  #       htmlOutput("loggedInAs"),
+                  #   ),
           
                   actionButton("login", "Login"),
-                  tags$h4("Logged in as:")
-                  #htmlOutput("loggedInAs", style="font-size:30;color:#D6AC18;font-family:Times New Roman;align:center;")
-                )
-              )
-      )
+                  tags$h4("Logged in as:"),
+                  htmlOutput("loggedInAs", style="font-size:30;color:#D6AC18;font-family:Times New Roman;align:center;")
+                ),
+      #           #Second tab content
+      #           tabItem(tabName = "onboarding",
+      #                   tags$div(
+      #                     tags$img(
+      #                       src='background.png',
+      #                       style="position: absolute;
+      #          top: -50%;
+      #          left: -50%;
+      #          width: 150%;
+      #          height: 150%;
+      #          display: flex;
+      #          z-order: 0;"
+      #                     ),
+      #                     tags$br(),
+      #                     tags$h1(
+      #                       'WELCOME!',
+      #                       style='color: #000000;
+      #            font-family: Times New Roman;
+      #            align: center;'
+      #                     ),
+      #                     
+      #                     
+      #                     # actionButton("login", "Login"),
+      #                     # tags$h4("Logged in as:"),
+      #                     # htmlOutput("loggedInAs", style="font-size:30;color:#D6AC18;font-family:Times New Roman;align:center;")
+      #                   )
+      #                   ,
+      # 
+      #   #third tab content
+      #   tabItem(tabName = "market",
+      #           tags$div(
+      #             tags$img(
+      #               src='background.png',
+      #               style="position: absolute;
+      #    top: -50%;
+      #    left: -50%;
+      #    width: 150%;
+      #    height: 150%;
+      #    display: flex;
+      #    z-order: 0;"
+      #             ),
+      #             tags$br(),
+      #             tags$h1(
+      #               'WELCOME!',
+      #               style='color: #000000;
+      #      font-family: Times New Roman;
+      #      align: center;'
+      #             ),
+      #             
+      #             
+      #             # actionButton("login", "Login"),
+      #             # tags$h4("Logged in as:"),
+      #             # htmlOutput("loggedInAs", style="font-size:30;color:#D6AC18;font-family:Times New Roman;align:center;")
+      #           )
+      #           ,
+      #   #fourth tab content
+      #   tabItem(tabName = "portfolio",
+      #           tags$div(
+      #             tags$img(
+      #               src='background.png',
+      #               style="position: absolute;
+      #            top: -50%;
+      #            left: -50%;
+      #            width: 150%;
+      #            height: 150%;
+      #            display: flex;
+      #            z-order: 0;"
+      #             ),
+      #             tags$br(),
+      #             tags$h1(
+      #               'WELCOME!',
+      #               style='color: #000000;
+      #              font-family: Times New Roman;
+      #              align: center;'
+      #             ),
+      #             
+      #             
+      #             # actionButton("login", "Login"),
+      #             # tags$h4("Logged in as:"),
+      #             # htmlOutput("loggedInAs", style="font-size:30;color:#D6AC18;font-family:Times New Roman;align:center;")
+      #           )                
+      #         )
+      # )
               
               
       )
       
-    )))
+    ))))
   
 
 ################SERVER####################
@@ -180,9 +282,10 @@ server <- function(input, output, session) {
   })
   # Fire some code if the user clicks the loginok button
   observeEvent(input$loginok, {
-    if (input$username=="dennis" && input$password=="abc") {
+    if (input$username=="dennis" && input$password=="lfg") {
       
       removeModal()
+      showModal(afterloginModal(failed=FALSE))
     } else {
       showModal(loginModal(failed = TRUE))
     }
@@ -194,13 +297,35 @@ server <- function(input, output, session) {
       "Not logged in yet."
     else
       input$username
+      
   })
   
+  observeEvent(input$viewportfolio, {
+    updateTabItems(session,"tabs","portfolio")
+  })
+  observeEvent(input$updateportfolio, {
+    updateTabItems(session,"tabs","portfolio")
+  })
+  observeEvent(input$onboardclients, {
+    updateTabItems(session,"tabs","onboarding")
+  })
+  observeEvent(input$marketoverview, {
+    updateTabItems(session,"tabs","market")
+  })
+   
   
 }
 
 
-
+# fluidRow(column(3,
+#                 actionButton("viewportfolio", "View Portfolio")),
+#          column(3,
+#                 actionButton("updateportfolio", "Update Portfolio")),
+#          column(3,
+#                 actionButton("onboardclients", "On Board Clients")),
+#          column(3,
+#                 actionButton("marketoverview"," Market Overview"))),
+# tags$br(),
 
 
 shinyApp(ui, server)
